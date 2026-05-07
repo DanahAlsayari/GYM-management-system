1935135d340dc006699ff79d1d8a09fff53bed43
DeemaAlhudaithi <deemaalhudaithy@gmail.com>
2026-05-08 01:54:41 +0300

    Update InsertMember.java

--- InsertMember.java
+++ InsertMember.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class InsertMember {
 
-    public static void insert(Member member) {
+    public static void insertMember(Member member) {
         String sql = "INSERT INTO MEMBER VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         try {
@@ -11,14 +11,14 @@ public class InsertMember {
 
             ps.setInt(1, member.getMemberID());
             ps.setString(2, member.getNationalID());
-            ps.setString(3, member.getFirstName());
-            ps.setString(4, member.getMiddleName());
-            ps.setString(5, member.getLastName());
-            ps.setString(6, member.getDateOfBirth());
-            ps.setString(7, member.getGender());
-            ps.setString(8, member.getAddress());
-            ps.setString(9, member.getPhoneNumber());
-            ps.setString(10, member.getEmail());
+            ps.setString(3, member.getPhoneNumber());
+            ps.setString(4, member.getFirstName());
+            ps.setString(5, member.getMiddleName());
+            ps.setString(6, member.getLastName());
+            ps.setString(7, member.getAddress());
+            ps.setString(8, member.getEmail());
+            ps.setString(9, member.getGender());
+            ps.setString(10, member.getDateOfBirth());
             ps.setInt(11, member.getBranchID());
             ps.setString(12, member.getSubscriptionID());
             ps.setString(13, member.getStartDate());
